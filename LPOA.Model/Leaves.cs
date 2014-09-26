d5f8707a9dc44de673d57163ae63fd5b1b84603b
Andy.Qi <qiliping@yeah.net>
2014-09-26 15:14:57 +0800

    完成请假WPFUI；完成请假流程设计；

---
+++ LPOA.Model/Leaves.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFramework.Extensions;
+
+namespace LPOA.Model
+{
+    public class Leave
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public string LeaveType { get; set; }
+
+        public string LeaveUser { get; set; }
+
+        public string Shangji { get; set; }
+
+        public string Bumen { get; set; }
+
+        public string Zongjingli { get; set; }
+
+
+        public string Intro { get; set; }
+    }
+
+    public class NorthwindContext : DbContext
+    {
+        public DbSet<Leave> Leaves { get; set; }
+
+        public void AddEntity(Leave entity)
+        {
+            Leaves.Add(entity);
+            base.SaveChanges();
+        }
+
+        public Leave GetById(long id)
+        {
+            return Leaves.Where(p => p.Id == id).FirstOrDefault();
+         
+        }
+    }
+}
